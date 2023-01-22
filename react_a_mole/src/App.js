e922b4b4b755de789322fa07cff9b9136c90882e
jwong65 <jason.wong6507@gmail.com>
2023-01-22 12:56:02 -0500

    Added the useState for the score

--- react_a_mole/src/App.js
+++ react_a_mole/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Usestate with the default score being 0
+  let [score, setScore] = useState(0)
   return (
-    <></>
+  <>
+  {/* This is the state for score. */}
+    {score}
+  </>
   );
 }
 
