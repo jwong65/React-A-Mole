b0031c865b15c6fc5269c8a1158863f2cd329b1d
jwong65 <jason.wong6507@gmail.com>
2023-01-22 13:19:20 -0500

    Added the displayMole into the return statement, not functional yet because the mole cannot be true or false yet

--- react_a_mole/src/components/MoleContainer.js
+++ react_a_mole/src/components/MoleContainer.js
@@ -7,8 +7,9 @@ function MoleContainer(){
   // State variable for the mole being displayed named displayMole
   //It needs to be ternary based on the assignment
   let displayMole = mole ?<Mole />: <EmptySlot />
+  //This will be passed into the return statement so the statement will present either a mole or the empty hill
     return(
-        <div style={{'display': 'inline-block', 'width': '15vw', 'margin': '1em'}}>Hi</div>
+        <div style={{'display': 'inline-block', 'width': '15vw', 'margin': '1em'}}>{displayMole}</div>
     )
 
 }
