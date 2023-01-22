7e112e1998f7c131179fc693c77ff7d0e0069176
jwong65 <jason.wong6507@gmail.com>
2023-01-22 13:17:32 -0500

    Imported both the mole/empty into container and had a terary conditional for if it is a mole or an empty slot

--- react_a_mole/src/components/MoleContainer.js
+++ react_a_mole/src/components/MoleContainer.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Mole from './Mole'
+import EmptySlot from './EmptySlot'
 
 function MoleContainer(){
+    
+  // State variable for the mole being displayed named displayMole
+  //It needs to be ternary based on the assignment
+  let displayMole = mole ?<Mole />: <EmptySlot />
     return(
         <div style={{'display': 'inline-block', 'width': '15vw', 'margin': '1em'}}>Hi</div>
     )
