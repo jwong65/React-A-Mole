000a39d1e5be73b83b3766284d5f094ec1899a25
jwong65 <jason.wong6507@gmail.com>
2023-01-22 12:51:48 -0500

    Added the components files with import/export

---
+++ react_a_mole/components/EmptySlot.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EmptySlot() {
+  return (
+    <div>EmptySlot</div>
+  )
+}
---
+++ react_a_mole/components/MoleContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Mole from './Mole'
+
+function MoleContainer(){
+
+}
+
+export default MoleContainer
