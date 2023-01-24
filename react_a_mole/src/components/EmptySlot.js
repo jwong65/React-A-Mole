ccfbb68c540f4c60e18ccff86d8043244afb84ee
jwong65 <jason.wong6507@gmail.com>
2023-01-24 18:55:34 -0500

    Added the useEffect to EmptySlot

--- react_a_mole/src/components/EmptySlot.js
+++ react_a_mole/src/components/EmptySlot.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function EmptySlot() {
+  //UseEffect with a callback function.
+  useEffect(()=>{
+    //Variable, assignment gave for random number
+    let randSeconds = Math.ceil(Math.random()*5000)
+    //set timer to setTimeout.
+    let timer = setTimeout(()=>{
+      props.setMoleHill(false)
+    }, randSeconds)
+  })
   return (
     <div>EmptySlot</div>
   )
