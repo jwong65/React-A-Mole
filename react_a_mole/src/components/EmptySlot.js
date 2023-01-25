8db240525839f51b214207263d39ea2af9bf0eb5
jwong65 <jason.wong6507@gmail.com>
2023-01-24 19:33:40 -0500

    Added the hill image and inserted the score onClick of the image

--- react_a_mole/src/components/EmptySlot.js
+++ react_a_mole/src/components/EmptySlot.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react'
+import moleHillImg from '../molehill.png'
 
-export default function EmptySlot() {
+export default function EmptySlot(props) {
   //UseEffect with a callback function.
   useEffect(()=>{
     //Variable, assignment gave for random number
-    let randSeconds = Math.ceil(Math.random()*5000)
+    let randSeconds = Math.ceil(Math.random()*2200)
     //set timer to setTimeout.
     let timer = setTimeout(()=>{
       props.setMoleHill(false)
     }, randSeconds)
   })
   return (
-    <div>EmptySlot</div>
+    <div>
+      <img style={{'wdith': '30vw'}}
+      src={moleHillImg}
+      //onClick will refer to MoleContainter
+      onClick={props.scoreCount}/>
+    </div>
   )
 }
